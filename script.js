1c15e4419a1d175fcc698c066e8bf82cf12d0ac8
AndrewHomtsiy <andriyko1395@gmail.com>
2020-07-18 12:08:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let product1 = 15.678;
-let product2 = 123.965;
-let product3 = 90.2345;
+const PRODUCT_1 = 15.678;
+const PRODUCT_2 = 123.965;
+const PRODUCT_3 = 90.2345;
 let Bank = 500;
 
-const priceList = [product1, product2, product3];
+const priceList = [PRODUCT_1, PRODUCT_2, PRODUCT_3];
 console.log('Products Price:' + priceList);
 
 const PriceMax = Math.max(...priceList);
@@ -12,14 +12,14 @@ const PriceMin = Math.min(...priceList);
 console.log('Min Price:' + PriceMin);
 const Sum = product1 + product2 + product3;
 console.log('Total:' + Sum);
-const NewSum = Math.floor(product1) + Math.floor(product2) + Math.floor(product3);
-console.log('New Price: ' + NewSum)
+const newSum = Math.floor(PRODUCT_1) + Math.floor(PRODUCT_2) + Math.floor(PRODUCT_3);
+console.log('New Price: ' + newSum)
 
-const newRoundSum = (Math.round(NewSum/100))*100 ;
+const newRoundSum = (Math.round(newSum/100))*100 ;
 document.writeln("Rounded to hundreds: " + newRoundSum); 
 
-const NewSumBoolean = Math.floor(NewSum) % 2 ? false : true;
-console.log("Boolean? " + NewSumBoolean);
+const newSumBoolean = Math.floor(NewSum) % 2 ? false : true;
+console.log("Boolean? " + newSumBoolean);
 
 const rest = Bank - Sum;
 console.log ('Rest: ' + rest);
@@ -31,18 +31,18 @@ const discount = Math.random()*100;
 console.log('Your discount is: ' + discount);
 const payprice = (Sum * (1 - discount / 100)).toFixed(2);
 console.log('Payprice is: ' + payprice);
-const Payprice2 = (Sum/2 - Sum*discount/100).toFixed(2);
-    console.log('Total is: ' + Payprice2);
+const payPrice2 = (Sum/2 - Sum*discount/100).toFixed(2);
+    console.log('Total is: ' + payPrice2);
 
     console.log(`
     Найвища ціна: ${PriceMax} грн <br>
     Найнижча ціна: ${PriceMin} грн <br>
     Сумарна вартість всіх товарів: ${Sum} грн <br>
-    Сумарна вартість всіх товарів без копійок: ${NewSum} грн <br>
-    Сума всіх товарів (округлена в меншу сторону) парна: ${NewSumBoolean} <br>
+    Сумарна вартість всіх товарів без копійок: ${newSum} грн <br>
+    Сума всіх товарів (округлена в меншу сторону) парна: ${newSumBoolean} <br>
     Решта з 500 грн: ${rest} грн <br>
     Середнє значення: ${MedianSum} грн <br>
     Сумарна вартість всіх товарів зі знижкою: ${payprice} грн <br>
-    Чистий прибуток: ${Payprice2} грн
+    Чистий прибуток: ${payPrice2} грн
 `);
 
